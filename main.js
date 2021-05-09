493edc7592834fe6855ceeb0bece0323b1c65fd2
Kae Okamura <kaehehehe@gmail.com>
2021-05-09 10:34:32 +0900

    Add preview feature

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const dragArea = document.querySelector('.drag-area');
 const dragText = document.querySelector('span');
 const icon = document.querySelector('.fas');
 
-dragArea.addEventListener('dragover', () => {
+let file;
+
+dragArea.addEventListener('dragover', (e) => {
+  e.preventDefault();
   dragArea.classList.add('active');
   icon.classList.add('fa-file-upload');
   icon.classList.remove('fa-file-image');
@@ -14,4 +17,27 @@ dragArea.addEventListener('dragleave', () => {
   icon.classList.add('fa-file-image');
   icon.classList.remove('fa-file-upload');
   dragText.textContent = 'Drag and drop here';
+});
+
+dragArea.addEventListener('drop', (e) => {
+  e.preventDefault();
+  file = e.dataTransfer.files[0];
+  const fileType = file.type;
+  
+  const validExtensions = ['image/jpeg', 'image/peg', 'image/png'];
+  if (validExtensions.includes(fileType)) {
+    const fileReader = new FileReader();
+      fileReader.onload = () => {
+      const fileURL = fileReader.result;
+      console.log(fileURL)
+      const imgTag = `<img src="${fileURL}" alt="">`;
+      dragArea.innerHTML = imgTag;
+      dragArea.classList.remove('active');
+      dragArea.classList.add('drop');
+    }
+    fileReader.readAsDataURL(file);
+  } else {
+    alert('This is not image file!');
+    dragArea.classList.remove('active');
+  }
 });
